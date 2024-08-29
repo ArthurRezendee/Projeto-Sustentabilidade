8a45f8e56150dc96d1b0d451a3f3d22abe9015d6
Rezende <arezendealmeida@gmail.com>
2024-08-29 02:59:16 -0300

    feat: requisição no forntend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -7,6 +8,7 @@ import google.generativeai as genai
 genai.configure(api_key=os.environ['API_KEY'])
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/ong/', methods=['POST'])
 def dados_produto():
